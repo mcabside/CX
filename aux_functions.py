1c585120f6b697b13cdd67ecf7bb895ab6193381
mcabside <mario.catapano@absidecorp.com>
2022-08-22 16:18:14 -0400

    Update aux_functions.py

--- aux_functions.py
+++ aux_functions.py
@@ -6,14 +6,14 @@ from cdc_questions import Preguntas_esfuerzo,Preguntas_satisfaccion,Preguntas_le
 def carga_preguntas(dataframe,CDC_Respuestas_Ref,Trimestre,Year):
     for row in range(len(dataframe)):
         data = ""
-        kpi_esfuerzo=0
-        kpi_esfuerzo_cont=0
-        kpi_satisfaccion=0
-        kpi_satisfaccion_cont=0
-        kpi_lealtad=0
-        kpi_lealtad_cont=0
-        kpi_valor=0
-        kpi_valor_cont=0
+        kpi_esfuerzo = 0
+        kpi_esfuerzo_cont = 0
+        kpi_satisfaccion = 0
+        kpi_satisfaccion_cont = 0
+        kpi_lealtad = 0
+        kpi_lealtad_cont = 0
+        kpi_valor = 0
+        kpi_valor_cont = 0
         for column in range(len(dataframe.columns)):
             colname = dataframe.columns[column]
             if dataframe.iloc[row,column] != "" and dataframe.iloc[row,column] != "No Aplica" and not pd.isna(dataframe.iloc[row,column]):
@@ -26,11 +26,31 @@ def carga_preguntas(dataframe,CDC_Respuestas_Ref,Trimestre,Year):
                 elif colname in Preguntas_lealtad:
                     kpi_lealtad += int(dataframe.iloc[row,column])
                     kpi_lealtad_cont +=1
-                elif colname in Preguntas_satisfaccion:
-                    kpi_satisfaccion += int(dataframe.iloc[row,column])
-                    kpi_satisfaccion_cont +=1
+                elif colname in Preguntas_esfuerzo:
+                    kpi_esfuerzo += int(dataframe.iloc[row,column])
+                    kpi_esfuerzo_cont +=1
+                
+                for pregunta in Preguntas_satisfaccion:
+                    if colname in pregunta:
+                        
+                        kpi_satisfaccion += int(dataframe.iloc[row,column])
+                        kpi_satisfaccion_cont += 1
+                
+                
+        kpi_valor = kpi_valor/kpi_valor_cont
+        kpi_lealtad = kpi_lealtad/kpi_lealtad_cont
+        kpi_esfuerzo = kpi_esfuerzo/kpi_esfuerzo_cont
+        print(kpi_valor)
+        print(kpi_lealtad)
+        print(kpi_esfuerzo)
+        print(data)
+        kpi_satisfaccion = kpi_satisfaccion/kpi_satisfaccion_cont
                 
         #kpi_lealtad = kp
+        data = data +str('"' + 'kpi_valor"' + ':' +'"'+ str(kpi_valor)+'"' + ',') 
+        data = data +str('"' + 'kpi_lealtad"' + ':' +'"'+ str(kpi_lealtad)+'"' + ',') 
+        data = data +str('"' + 'kpi_satisfaccion"' + ':' +'"'+ str(kpi_satisfaccion)+'"' + ',') 
+        data = data +str('"' + 'kpi_esfuerzo"' + ':' +'"'+ str(kpi_esfuerzo)+'"' + ',') 
 
         data = data +str('"' + 'Trimestre"' + ':' +'"'+ str(Trimestre)+'"' + ',') 
 
