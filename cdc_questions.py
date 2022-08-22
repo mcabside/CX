37e4f1caa5ac123f2210e1be6dd138c890b9847c
dmabside <111077940+dmabside@users.noreply.github.com>
2022-08-22 16:21:24 -0400

    Quitar preguntas duplicadas

--- cdc_questions.py
+++ cdc_questions.py
@@ -2,19 +2,9 @@ Preguntas_satisfaccion = ["Con respecto a los siguientes servicios entregados po
 "Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Servicios Proactivos de Requerimientos]",    
 "Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Proveer información oportuna para su empresa]",    
 "Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Gestión preventiva sobre su plataforma]",    
-"Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Gestión de Fallas (Incidentes)]",    
-"Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Asesoría y Soporte]",    
-"Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Gestión preventiva sobre su plataforma]",    
-"Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Proveer información oportuna para su empresa]",    
-"Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Servicios de Innovación de Soporte (ej: Readiness Check, PathFinder, Prog. Adopción S/4 HANA, Transf. Navigator, entre otros)]",    
 "Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Servicios de aprendizaje (ejem: Workshop, Mesas Técnicas, Training, entre otros)]",    
 "Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Gestión de Fallas (Incidentes)]",    
-"Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Asesoría y Soporte]",    
-"Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Gestión preventiva sobre su plataforma]",    
-"Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Servicios Proactivos de Requerimientos]",    
-"Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Proveer información oportuna para su empresa]",    
 "Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Servicios de Innovación de Soporte (ej: Readiness Check, PathFinder, Prog. Adopción S/4 HANA, Transf. Navigator, entre otros)]",    
-"Con respecto a los siguientes servicios entregados por el Centro de Competencias de ABSIDE, evalúe su grado de satisfacción: [Servicios de aprendizaje (ejem: Workshop, Mesas Técnicas, Training, entre otros)]"
 ]
 Preguntas_valor = ["¿Cuánto valor aportan los servicios brindados por el Centro de Competencias de ABSIDE para gestionar la plataforma SAP de su empresa?"]
            
