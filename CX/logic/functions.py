156fb88c982c7edfbff2cc7502fa8d596569bd56
mcabside <mario.catapano@absidecorp.com>
2022-10-18 12:52:02 -0400

    ajuste de ortografia

--- CX/logic/functions.py
+++ CX/logic/functions.py
@@ -418,7 +418,7 @@ def getHistorico(Datos,Cliente,KPI,Area):
         valores.append(valor[2])
     
     
-    fig = px.line(x=periodos, y=valores,labels={'x':'periodos', 'y':KPI[4:]}, title='Historico ' + KPI[4:] + " " + Cliente + " " + Area,
+    fig = px.line(x=periodos, y=valores,labels={'x':'períodos', 'y':KPI[4:]}, title='Historico ' + KPI[4:] + " " + Cliente + " " + Area,
                   markers=True,text=valores)
     fig.update_layout(yaxis_range=[0,10])
     fig.update_traces(textposition="bottom center")
